2f82b45d6507f331d0df7cbf418b19f1504ca8b6
FameZeeshan <zeeshan6143@gmail.com>
2022-09-27 18:15:33 +0530

    form validation

--- script/script.js
+++ script/script.js
@@ -1,2 +1,13 @@
 "use strict";
 // This will give more power to JS , helps to ignore errors in JS
+
+// selecting HTML Elements in JS
+// Modifying the elements selected
+// Create and add elements in HTML using JS
+
+// selecting HTML Elements in JS:
+// document.querySelector("")
+// document.getElementById("")
+// Use classes for CSS and Id for Javascript
+
+console.log(document.getElementById("title"));
