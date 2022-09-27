4027190e244f65652d0a56f0a0f451568ac119a2
FameZeeshan <zeeshan6143@gmail.com>
2022-09-27 23:19:17 +0530

    add required logic

--- script/script.js
+++ script/script.js
@@ -3,11 +3,69 @@
 
 // selecting HTML Elements in JS
 // Modifying the elements selected
+// document.getElementById("title").textContent = "Fill the Form";
 // Create and add elements in HTML using JS
+// const newEl = document.createElement("h2");
+// newEl.textContent = "Welcome to My Book";
+// newEl.style.textAlign = "center";
+// document.querySelector("#contain").appendChild(newEl);
 
 // selecting HTML Elements in JS:
 // document.querySelector("")
 // document.getElementById("")
 // Use classes for CSS and Id for Javascript
 
-console.log(document.getElementById("title"));
+// document.getElementById("title").style.color = "pink";
+// -----------------------xx--------------
+// Select Elements:-
+const formEl = document.getElementById("form");
+const usernameEl = document.getElementById("username");
+const emailEl = document.getElementById("email");
+const mobileEl = document.getElementById("mobile");
+const newPasswordEl = document.getElementById("new-password");
+const confirmPasswordEl = document.getElementById("confirm-password");
+
+// Events:-
+// AddEventListener:
+formEl.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (usernameEl.value === "") {
+    const formControlEl = usernameEl.parentElement;
+    formControlEl.classList.add("error");
+  } else {
+    const formControlEl = usernameEl.parentElement;
+    formControlEl.classList.add("success");
+  }
+
+  if (emailEl.value === "") {
+    const formControlEl = emailEl.parentElement;
+    formControlEl.classList.add("error");
+  } else {
+    const formControlEl = emailEl.parentElement;
+    formControlEl.classList.add("success");
+  }
+
+  if (mobileEl.value === "") {
+    const formControlEl = mobileEl.parentElement;
+    formControlEl.classList.add("error");
+  } else {
+    const formControlEl = mobileEl.parentElement;
+    formControlEl.classList.add("success");
+  }
+
+  if (newPasswordEl.value === "") {
+    const formControlEl = newPasswordEl.parentElement;
+    formControlEl.classList.add("error");
+  } else {
+    const formControlEl = newPasswordEl.parentElement;
+    formControlEl.classList.add("success");
+  }
+
+  if (confirmPasswordEl.value === "") {
+    const formControlEl = confirmPasswordEl.parentElement;
+    formControlEl.classList.add("error");
+  } else {
+    const formControlEl = confirmPasswordEl.parentElement;
+    formControlEl.classList.add("success");
+  }
+});
